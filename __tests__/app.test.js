9b373406d60e4ade30df1f132b74e362af8b3685
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-04-15 10:52:07 -0700

    get image by ID method & test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,12 +19,21 @@ describe('foamyBE routes', () => {
     expect(res.status).toEqual(404);
   });
 
-  it('should get all images', async() => {
+  it('get all images', async() => {
     const res = await request(app).get('/api/v1/images/')
       .expect('Content-Type', /json/)
       .expect(200);
     expect(res.body).toEqual(expect.arrayContaining([{ 'foamy': null, 'id': 1, 'last_modified': '2022-02-23T21:31:27.000Z', 'url': 'https://take-home-foam-challenge.s3.us-west-2.amazonaws.com/prod-exp13436-2020-01-08-at-04.24.38-9zijoye9dteugy6agooo506u3c6wrin920a99mavvv4z9mahkt7qbu6thl2l3v39.png' }]));
   });
-  // A small change in order to ACP
+
+  it('get image by ID', async() => {
+    const res = await request(app).get('/api/v1/images/5')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(res.body).toEqual(
+      { 'foamy': null, 'id': 5, 'last_modified': '2022-02-23T21:31:27.000Z', 'url': 'https://take-home-foam-challenge.s3.us-west-2.amazonaws.com/prod-exp13436-2020-01-08-at-04.28.52-b0l43ldf7drejbvb9zwwdfk6mzrijepfskmd2fe2nnkypep4oykasytfad4jgs65.png' }
+    );
+  });
+
   
 });
