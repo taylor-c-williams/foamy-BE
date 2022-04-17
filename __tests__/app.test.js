aeff56e8947fe1e9c97df74fdb54ba400bc85de9
Taylor <taylor.callan.williams@gmail.com>
2022-04-16 18:47:07 -0700

    Revert "server side pagination"

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,10 +19,10 @@ describe('foamyBE routes', () => {
   });
 
   it('get all images', async() => {
-    const res = await request(app).get('/api/v1/images?perPage=1&pageNumber=2')
+    const res = await request(app).get('/api/v1/images/')
       .expect('Content-Type', /json/)
       .expect(200);
-    expect(res.body).toEqual(expect.arrayContaining([{ 'foamy': null, 'id': 2, 'last_modified': '2022-02-23T21:31:27.000Z', 'url': 'https://take-home-foam-challenge.s3.us-west-2.amazonaws.com/prod-exp13436-2020-01-08-at-04.25.42-0fw8tob3vhrhl8u0f3mu2g37bwnojkpwiaku4rfi3xk868y7dygr2hbge1oenzmc.png' }]));
+    expect(res.body).toEqual(expect.arrayContaining([{ 'foamy': null, 'id': 1, 'last_modified': '2022-02-23T21:31:27.000Z', 'url': 'https://take-home-foam-challenge.s3.us-west-2.amazonaws.com/prod-exp13436-2020-01-08-at-04.24.38-9zijoye9dteugy6agooo506u3c6wrin920a99mavvv4z9mahkt7qbu6thl2l3v39.png' }]));
   });
 
   it('get image by ID', async() => {
